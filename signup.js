0fe8338e6f249f6c090d7ebfdda5587958b0e8df
Nandithu <nanditha7prabhu@gmail.com>
2023-08-12 00:02:08 +0530

    sign up, login changes, logo, 8 avatars

--- signup.js
+++ signup.js
@@ -1,15 +1,5 @@
-// const close = document.getElementById("close");
-// const open = document.getElementById("open");
-// const modal = document.getElementById("form-box");
-
-// open.addEventListener("click", ()=>modal.classList.add("show-modal"));
-
-// close.addEventListener("click", ()=>modal.classList.remove("show-modal"));
-
-// window.addEventListener("click", (e)=>{
-//     e.target === modal ? modal.classList.remove("show-modal") : false ;
-// })
-
+// modal is the element that pops up from the page
+// for sign up form
 function showModal() {
     document.querySelector('.overlay').classList.add('show-overlay');
     document.querySelector('.form-content').classList.add('show-form-content');
@@ -18,9 +8,25 @@ function closeModal() {
     document.querySelector('.overlay').classList.remove('show-overlay');
     document.querySelector('.form-content').classList.remove('show-form-content');
 }
-
 var btnSignUp = document.querySelector('#btn-signup');
 btnSignUp.addEventListener("click", showModal);
-
-var closeBtn = document.querySelector('span');
+var closeBtn = document.querySelector('#span-signup');
 closeBtn.addEventListener('click', closeModal);
+
+// for login form
+function showModal2() {
+    // first show overlay
+    document.querySelector('.overlay2').classList.add('show-overlay');
+    // then content
+    document.querySelector('.form-content2').classList.add('show-form-content');
+}
+function closeModal2() {
+    // first show overlay
+    document.querySelector('.overlay2').classList.remove('show-overlay');
+    // then content
+    document.querySelector('.form-content2').classList.remove('show-form-content');
+}
+var btnLogin = document.querySelector("#btn-login");
+btnLogin.addEventListener("click", showModal2);
+var closeBtn = document.querySelector('#span-login');
+closeBtn.addEventListener('click', closeModal2);
